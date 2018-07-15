9522f21447b4ecaa368a87ea92b4e794903cfb62
Tao <werewaswas@live.com>
2018-07-15 01:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.startDate =  new Date(date);
+  }
 
+  yearsExperienceFromBeginningOf(endYear) {
+    const endDate = new Date(endYear, 1, 1);
+    const years = (endDate - this.startDate)/1000/60/60/24/365; //ms to year
+    return parseInt(years) + 1;
+  }
+}
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+endingLocation.horizontal - beginningLocation.horizontal 
+}
