2bd7fa7968cb8da295e14856e5bf9584a8ec3cda
Tao <werewaswas@live.com>
2018-07-15 01:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ class Route {
   blocksTravelled() {
     const horizontal = this.avenueToInt(this.endingLocation.horizontal) - this.avenueToInt(this.beginningLocation.horizontal);
     const vertical = parseInt(this.endingLocation.vertical) - parseInt(this.beginningLocation.vertical);
-    return (horizontal >= 0) ? horizontal : horizontal - 2 * horizontal + vertical;
+    return (horizontal >= 0) ? horizontal : horizontal - 2 * horizontal + (vertical >= 0) ? vertical : vertical - 2 * vertical;
   }
 
   estimatedTime() {
